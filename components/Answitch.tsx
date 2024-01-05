f02a6c61ee5ab7e0d53dd6872befeb7a01f4b5b3
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-01-05 19:12:48 +0100

    added stuff

--- components/Answitch.tsx
+++ components/Answitch.tsx
@@ -2,10 +2,8 @@
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { motion } from 'framer-motion'
-interface AnswitchProps {
-    toggleTheme: boolean;
-}
-export default function Answitch({toggleTheme} : AnswitchProps) {
+
+export default function Answitch() {
     const [mounted, setMounted] = useState(false)
     const [toggle, setToggle] = useState(false)
     const { theme, setTheme } = useTheme()
@@ -14,22 +12,19 @@ export default function Answitch({toggleTheme} : AnswitchProps) {
     useEffect(() => {
       setMounted(true)
     }, [])
-    useEffect(() => {
-        if (mounted) {
-          if (theme === "dark") {
-            setTheme("light")
-          } else {
-            setTheme("dark")
-          }
-        }
-      }, [toggleTheme, theme])
+    
     if (!mounted) {
       return null
     }
-  
+    
+    const handleThemeToggle = () => {
+      const newTheme = theme === 'dark' ? 'light' : 'dark';
+      setToggle(!toggle);
+      setTheme(newTheme);
+    };
     return (
-      <div className='mb-10'>
-        <div onClick={() => setToggle(!toggle)} className={`flex h-6 w-12 cursor-pointer rounded-full border 
+      <div className=''>
+        <div onClick={handleThemeToggle} className={`flex h-6 w-12 cursor-pointer rounded-full border 
         ${toggle ? 'justify-start' : 'justify-end'} bg-[rgba(99,95,199,255)] 
         border-[rgba(99,95,199,255)] hover:bg-[rgb(146,143,232)] p-[1px]`}>
         <motion.div
