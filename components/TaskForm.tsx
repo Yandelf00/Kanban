bf48ef93a58f3aa0c54243c7222d1fe8f167a308
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2024-01-08 21:41:34 +0100

    added stuff

--- components/TaskForm.tsx
+++ components/TaskForm.tsx
@@ -1,10 +1,23 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import { Tasks } from '.';
+import { useSelector } from 'react-redux';
+import { boardType, columnType } from '@/store';
+import { title } from 'process';
 
 
 export default function TaskForm() {
     const [subTasks, setSubTasks] = useState({num : 2, tasks : [{id : 1}, {id : 2}]})
+    const boards = useSelector((state:any)=>state.boards.boards)
+    const activeBoard = boards.find((board : boardType)=>board.isActive === true)
+    const title = useRef<HTMLInputElement>(null);
+    const description = useRef<HTMLTextAreaElement>(null);
+    const subtaskRefs = useRef<Array<HTMLInputElement|null>>([]);
+    const selectRef = useRef<HTMLSelectElement>(null);
+
+    // if (subtaskRefs.current.length !== subTasks.num) {
+    //     subtaskRefs.current = Array(subTasks.num).fill(null).map((_, index) => subtaskRefs.current[index] || null);
+    // }
     function addSubtasks(){
         const newId = subTasks.num + 1;
         setSubTasks((prevSubTasks) => {
@@ -13,6 +26,7 @@ export default function TaskForm() {
                 tasks: [...prevSubTasks.tasks, { id: newId }],
             } 
         })
+        // subtaskRefs.current.push(null);
     }
     function deleteSubtasks(id : number){
         const updatedSubtasks = subTasks.tasks.filter((subtask)=>subtask.id !== id)
@@ -22,7 +36,15 @@ export default function TaskForm() {
                 tasks : updatedSubtasks
             }
         })
+        // subtaskRefs.current.pop();
     }
+    function Essai() {
+        const liste:string[] = []
+        subtaskRefs.current.forEach((subtaskRef) => {
+            subtaskRef ? liste.push(subtaskRef?.value) : '';
+        });
+        console.log(liste)
+    } 
     return (
         <div className='h-[90%] w-[90%] flex flex-col'>
             <h1 className='font-bold text-[1.2rem]'>Add New Task</h1>
@@ -31,6 +53,7 @@ export default function TaskForm() {
                     Title
                 </label>
                 <input
+                ref = {title}
                 type="text"
                 id="taskName"
                 placeholder="e.g. Take coffee break"
@@ -39,19 +62,20 @@ export default function TaskForm() {
                 <label htmlFor="longTextInput" className="mb-2 text-black dark:text-white">
                     Description
                 </label>
-                <textarea className='border dark:border-none border-black' 
+                <textarea ref={description} className='border dark:border-none border-black' 
                 id="longTextInput" 
                 name="longInput" style={{ width: '100%', height: '100px' }}></textarea>
                 <label>
                     Subtasks
                 </label>
                 <div className='flex flex-col'>
-                    {subTasks.tasks.map((subtask) => (
+                    {subTasks.tasks.map((subtask, index) => (
                         <div key={subtask.id} className="flex items-center">
                         <input
                           type="text"
                           placeholder=''
                           className="p-2 mt-2 rounded-md focus:outline-none focus:border-blue-500"
+                          ref={(inputRef) => (subtaskRefs.current[index] = inputRef)}
                         />
                         <button
                           type="button"
@@ -64,6 +88,15 @@ export default function TaskForm() {
                     ))}
                 </div>
                 <button onClick={addSubtasks}>add</button>
+
+                <label htmlFor="mySelect">Status</label>
+                <select ref={selectRef} id="mySelect" name="select">
+                    {activeBoard.columns.map((column:columnType)=>(
+                        <option value={column.name}>{column.name}</option>
+                    ))}
+                </select>
+                
+                <button onClick={Essai}>create task</button>
                 
 
 
